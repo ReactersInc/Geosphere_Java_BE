a2ad603088f645103a36c78215079371ded67462
Rajveer6000 <csb21075@tezu.ac.in>
2025-04-16 00:20:55 +0530

    Update Validation

---
+++ src/main/java/com/tridev/geoSphere/constant/ErrorConstants.java
@@ -0,0 +1,38 @@
+package com.tridev.geoSphere.constant;
+
+public class ErrorConstants {
+
+    public static final String INVALID_CREDENTIALS = "401";
+    public static final String INVALID_CREDENTIALS_DESC = "Invalid Credentials";
+
+    public static final String ACCOUNT_ALREADY_EXIST = "1000";
+    public static final String ACCOUNT_ALREADY_EXIST_DESC = "Account Already Exist";
+
+    public static final String INVALID_ACCOUNT_NUMBER = "1001";
+    public static final String INVALID_ACCOUNT_NUMBER_DESC = "Invalid Account Number";
+
+    public static final String INSUFFICIENT_FUNDS = "1002";
+    public static final String INSUFFICIENT_FUNDS_DESC = "Insufficient funds";
+
+    public static final String FROM_ACCOUNT_DOES_NOT_EXIST = "1003";
+    public static final String FROM_ACCOUNT_DOES_NOT_EXIST_DESC = "From Account does not exist";
+
+    public static final String TO_ACCOUNT_DOES_NOT_EXIST = "1004";
+    public static final String TO_ACCOUNT_DOES_NOT_EXIST_DESC = "To Account does not exist";
+
+    public static final String INCORRECT_MPIN = "1005";
+    public static final String INCORRECT_MPIN_DESC = "Incorrect MPIN " ;
+
+    public static final String INVALID_MPIN = "1006";
+    public static final String INVALID_MPIN_DESC = "MPIN must be exactly 6 digits" ;
+
+    public static final String CURRENT_MPIN_MISMATCH = "1007";
+    public static final String CURRENT_MPIN_MISMATCH_DESC= "Current MPIN is not matching";
+
+    public static final String RESET_MPIN_ERROR = "1008";
+    public static final String RESET_MPIN_ERROR_DESC = "Current MPIN And new MPIN can not be same";
+
+    public static final String MPIN_PRESENT = "1009";
+    public static final String MPIN_PRESENT_DESC= "MPIN is already present";
+
+}
