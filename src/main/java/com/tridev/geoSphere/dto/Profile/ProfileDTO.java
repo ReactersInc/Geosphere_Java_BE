e3a2dbd5abb51cab0482f699a54dc63ffcd0fd19
Rajveer <csb21075@tezu.ac.in>
2025-05-11 16:03:10 +0530

    Update Profile Service

--- src/main/java/com/tridev/geoSphere/dto/Profile/ProfileDTO.java
+++ src/main/java/com/tridev/geoSphere/dto/Profile/ProfileDTO.java
@@ -1,5 +1,6 @@
 package com.tridev.geoSphere.dto.Profile;
 
+import com.tridev.geoSphere.dto.User.UserGeofenceDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,6 @@ public class ProfileDTO {
     private String email;
     private String firstName;
     private String lastName;
+    private UserGeofenceDTO userGeofenceDetails;
 
 }
--- src/main/java/com/tridev/geoSphere/services/ProfileService.java
+++ src/main/java/com/tridev/geoSphere/services/ProfileService.java
@@ -2,25 +2,45 @@ package com.tridev.geoSphere.services;
 
 import com.tridev.geoSphere.constant.CommonValidationConstant;
 import com.tridev.geoSphere.dto.Profile.ProfileDTO;
+import com.tridev.geoSphere.dto.User.GeofenceDetailsDTO;
+import com.tridev.geoSphere.dto.User.UserGeofenceDTO;
+import com.tridev.geoSphere.entities.sql.GeofenceEntity;
 import com.tridev.geoSphere.entities.sql.UserEntity;
+import com.tridev.geoSphere.entities.sql.UserGeofenceEntity;
+import com.tridev.geoSphere.enums.ResponseStatus;
+import com.tridev.geoSphere.enums.Status;
 import com.tridev.geoSphere.exceptions.BadRequestException;
 import com.tridev.geoSphere.mappers.UserMapper;
-import com.tridev.geoSphere.repositories.sql.UserRepo;
+import com.tridev.geoSphere.repositories.sql.*;
 import com.tridev.geoSphere.response.BaseResponse;
 import com.tridev.geoSphere.utils.GeosphereServiceUtility;
 import com.tridev.geoSphere.utils.JwtUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProfileService {
-
-
-
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private UserGeofenceRepository userGeofenceRepo;
+
+    @Autowired
+    private GeofenceRepository geofenceRepo;
+
+    @Autowired
+    private UserContactsRepository userContactsRepo;
+
+    @Autowired
+    private GeofenceRequestRepository geofenceRequestRepo;
+
+    @Autowired
+    private ConnectionRequestRepository connectionRequestRepo;
 
     @Autowired
     private UserMapper registerUserMapper;
@@ -30,30 +50,69 @@ public class ProfileService {
     private JwtUtil jwtUtil;
 
 
+    @Transactional
     public BaseResponse findUserByUserId() throws BadRequestException {
-
         Long userId = jwtUtil.getUserIdFromToken();
-
         Optional<UserEntity> entity = userRepo.findById(userId);
 
-        if(entity.isPresent()){
-
-            ProfileDTO  user =  registerUserMapper.toProfile(entity.get());
-
-            return GeosphereServiceUtility.getBaseResponse(user);
+        if (entity.isEmpty()) {
+            throw new BadRequestException(CommonValidationConstant.USER_NOT_FOUND);
         }
 
+        UserEntity user = entity.get();
+        ProfileDTO profileDTO = new ProfileDTO();
+        profileDTO.setEmail(user.getEmail());
+        profileDTO.setFirstName(user.getFirstName());
+        profileDTO.setLastName(user.getLastName());
 
-        throw new BadRequestException(CommonValidationConstant.USER_NOT_FOUND);
+        // Prepare UserGeofenceDTO
+        UserGeofenceDTO userGeofenceDTO = new UserGeofenceDTO();
+
+        // Total Geofences
+        Integer totalGeofence = userGeofenceRepo.countByUserId(userId);
+        Integer totalGeofenceInUse = userGeofenceRepo.countByUserIdAndStatus(userId, Status.ACTIVE.getValue());
+        Integer totalGeofenceNotInUse = totalGeofence - totalGeofenceInUse;
+
+        // Total users in geofence = distinct userId count in UserGeofenceEntity
+        List<Long> geofenceIds = userGeofenceRepo.findAllGeofenceIdsByUserId(userId);
+        Integer totalUsersInGeofence = geofenceIds.isEmpty() ? 0 :
+                userGeofenceRepo.countDistinctUserIdByGeofenceIds(geofenceIds);
 
 
+        // Total connections
+        Integer totalConnections = userContactsRepo.countByUserIdAndStatus(userId, Status.ACTIVE.getValue());
 
+        // Pending geofence requests
+        Integer pendingGeofenceRequests = geofenceRequestRepo.countByUserIdAndResponseStatus(userId, ResponseStatus.PENDING);
+
+        // Pending connection requests
+        Integer pendingConnectionRequests = connectionRequestRepo.countByTargetUserIdAndStatus(userId, Status.PENDING.getValue());
+
+        // Currently inside geofence
+        Optional<UserGeofenceEntity> currentGeofenceOpt = userGeofenceRepo
+                .findFirstByUserIdAndIsCurrentlyInsideTrue(userId);
+
+        GeofenceDetailsDTO inWitchGeofence = null;
+        if (currentGeofenceOpt.isPresent()) {
+            Long geofenceId = currentGeofenceOpt.get().getGeofenceId();
+            Optional<GeofenceEntity> geofenceEntity = geofenceRepo.findById(geofenceId);
+            if (geofenceEntity.isPresent()) {
+                GeofenceEntity gf = geofenceEntity.get();
+                inWitchGeofence = new GeofenceDetailsDTO(gf.getId(), gf.getName(), gf.getDescription());
+            }
+        }
+
+        userGeofenceDTO.setTotalGeofence(totalGeofence);
+        userGeofenceDTO.setTotalGeofenceInUse(totalGeofenceInUse);
+        userGeofenceDTO.setTotalGeofenceNotInUse(totalGeofenceNotInUse);
+        userGeofenceDTO.setTotalUsersInGeofence(totalUsersInGeofence);
+        userGeofenceDTO.setTotalConnections(totalConnections);
+        userGeofenceDTO.setPendingGeofenceRequests(pendingGeofenceRequests);
+        userGeofenceDTO.setPendingConnectionRequests(pendingConnectionRequests);
+        userGeofenceDTO.setInWitchGeofence(inWitchGeofence);
+
+        profileDTO.setUserGeofenceDetails(userGeofenceDTO);
+
+        return GeosphereServiceUtility.getBaseResponse(profileDTO);
     }
-
-
-
-
-
-
-
 }
