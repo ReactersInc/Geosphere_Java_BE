7a8662fb25317924a87e71849737abc501266474
Rajveer <csb21075@tezu.ac.in>
2025-05-12 18:57:12 +0530

    Add get Profile and Update profile

--- src/main/java/com/tridev/geoSphere/controllers/ProfileController.java
+++ src/main/java/com/tridev/geoSphere/controllers/ProfileController.java
@@ -1,12 +1,11 @@
 package com.tridev.geoSphere.controllers;
 
+import com.tridev.geoSphere.dto.User.UserDetailsDTO;
 import com.tridev.geoSphere.exceptions.BadRequestException;
 import com.tridev.geoSphere.response.BaseResponse;
 import com.tridev.geoSphere.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
@@ -22,5 +21,10 @@ public class ProfileController {
 
         return profileService.findUserByUserId();
 
+    }
+
+    @PutMapping
+    public BaseResponse updateUserProfile(@RequestBody UserDetailsDTO userDetailsDTO) throws BadRequestException {
+        return profileService.updateUserProfile(userDetailsDTO);
     }
 }
--- src/main/java/com/tridev/geoSphere/dto/Profile/ProfileDTO.java
+++ src/main/java/com/tridev/geoSphere/dto/Profile/ProfileDTO.java
@@ -5,14 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileDTO {
-
+    private Long id;
     private String email;
     private String firstName;
     private String lastName;
+    private Boolean publicProfile;
+    private Boolean isVerified;
+    private String photoUrl;
+    private LocalDateTime createdAt;
     private UserGeofenceDTO userGeofenceDetails;
 
 }
