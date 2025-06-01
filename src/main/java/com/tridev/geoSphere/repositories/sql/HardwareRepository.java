ae96e6c549b4ecd226bee6fdba423268bfaa38d8
Rajveer <csb21075@tezu.ac.in>
2025-06-01 23:56:24 +0530

    add hardware location update api

--- src/main/java/com/tridev/geoSphere/repositories/sql/HardwareRepository.java
+++ src/main/java/com/tridev/geoSphere/repositories/sql/HardwareRepository.java
@@ -3,7 +3,11 @@ package com.tridev.geoSphere.repositories.sql;
 import com.tridev.geoSphere.entities.sql.HardwareEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HardwareRepository extends JpaRepository<HardwareEntity, Long> {
     Boolean existsByMacAddressAndStatusId(String macAddress, Integer value);
     HardwareEntity findByIdAndStatusId(Long hardwareId, Integer value);
+
+    Optional<HardwareEntity> findByMacAddressAndStatusId(String macAddress, Integer value);
 }
